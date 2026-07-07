5e79d841942955b0c6443a3563bb4d9615be0998
sahilrai0201 <sahilatwork21@gmail.com>
2026-07-07 18:19:24 +0530

    feat: initialize backend project with Express, Mongoose dependencies and MongoDB connection utility

--- Backend/db.js
+++ Backend/db.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose";
+import dns from "dns";
+
+// Override DNS servers to Google and Cloudflare to resolve SRV records properly
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
     try {
